2aa4074d193536a0a47605c7dd83475338565a6e
Miguegithub78 <avallemiguel@hotmail.com>
2021-12-16 01:12:40 -0300

    saco lessons

--- api/src/models/index.js
+++ api/src/models/index.js
@@ -1,11 +1,9 @@
 const Cursos = require('./collections/curses');
 const Teachers = require('./collections/teachers');
-const Lessons = require('./collections/lessons');
 const Students = require('./collections/students');
 
 module.exports = {
   Teachers,
   Cursos,
-  Lessons,
   Students
 };
